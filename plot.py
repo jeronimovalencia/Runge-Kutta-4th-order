10fb9ec951a2ab7a9d50e85b3fba02514a730e33
Jerónimo Valencia Porras <40802479+jeronimovalencia@users.noreply.github.com>
2018-07-10 16:55:02 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,5 +4,8 @@ import numpy as np
 datosPendulo = np.genfromtxt("datosPendulo.dat")
 
 plt.figure()
+plt.subplot(211)
 plt.plot(datosPendulo[:,0],datosPendulo[:,1], label = "Pendulo")
+plt.subplot(212)
+plt.scatter(np.sin(datosPendulo[:,1]),-np.cos(datosPendulo[:,1]))
 plt.savefig("graficas.pdf")
